0641314cb77f00ec626f281466b4d8c85c60f8b8
NikDzub <87159434+NikDzub@users.noreply.github.com>
2025-03-04 20:12:49 +0200

    .

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import {
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import destinations from './destinations.json';
-import AirportMarker from './components/AirportMarker'; // Import the reusable component
+import AirportMarker from './components/AirportMarker';
+import Quiz from './components/Quiz'; // Import Quiz component
 
-// Green icon for primary location (Tel Aviv)
 const blueIcon = new L.Icon({
   iconUrl:
     'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
@@ -24,38 +24,37 @@ const primaryLocation = [32.0055, 34.8854];
 
 const App = () => {
   return (
-    <MapContainer
-      center={primaryLocation}
-      zoom={5}
-      style={{ width: '100%', height: '100vh' }}
-    >
-      {/* <TileLayer
-        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-        attribution="&copy; <a href='https://www.esri.com'>Esri</a>, &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-      /> */}
+    <div className="app-container">
+      <MapContainer
+        center={primaryLocation}
+        zoom={5}
+        style={{ width: '100%', height: '100vh' }}
+      >
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
 
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
+        <Marker position={primaryLocation} icon={blueIcon}>
+          <Popup>Tel Aviv Ben Gurion Airport</Popup>
+        </Marker>
 
-      <Marker position={primaryLocation} icon={blueIcon}>
-        <Popup>Tel Aviv Ben Gurion Airport</Popup>
-      </Marker>
+        {destinations.map((airport, index) => (
+          <React.Fragment key={index}>
+            <AirportMarker airport={airport} />
+            <Polyline
+              positions={[primaryLocation, airport.coords]}
+              color="grey"
+              weight={0.8}
+              opacity={0.4}
+            />
+          </React.Fragment>
+        ))}
+      </MapContainer>
 
-      {/* Airport Markers */}
-      {destinations.map((airport, index) => (
-        <React.Fragment key={index}>
-          <AirportMarker airport={airport} />
-          <Polyline
-            positions={[primaryLocation, airport.coords]}
-            color="grey"
-            weight={0.8}
-            opacity={0.4}
-          />
-        </React.Fragment>
-      ))}
-    </MapContainer>
+      {/* Quiz Component at the Bottom */}
+      <Quiz />
+    </div>
   );
 };
 
